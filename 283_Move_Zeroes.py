15dd916c0b784c5d1ebd8967f7d755d6f0d08eff
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-09 20:39:41 +0530

    Sync: 283. Move Zeroes (Easy) - Leet2Git

---
+++ 283_Move_Zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j =1
+        i =0
+        while j < len(nums):
+            if nums[i] == 0 and nums[j] != 0:
+                nums[i] ,nums[j] =nums[j] ,nums[i]
+                i+=1
+            j+=1
