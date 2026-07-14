5affb6fe6160ac8d5c8a17c5ef0b169f0eb0d4cd
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-14 20:57:41 +0530

    Sync: 39. Combination Sum (Medium) - Leet2Git

--- 39_Combination_Sum.py
+++ 39_Combination_Sum.py
@@ -1,22 +1,45 @@
 class Solution:
     def combinationSum(self, candidates, target):
-        final =[] 
-        subset =[]
+        final  = []
+        sub = []
         candidates.sort()
-        def solve(index ,subset  ,target):
-
+        def solve(index ,sub ,target):
+            i
             if target == 0:
-                final.append(subset.copy())
+                final.append(sub.copy())
                 return
             
-            for i in range (index ,len(candidates)):
-                if candidates[i] >target :
+            
+            for i in range(index ,len(candidates)):
+                if candidates[i] > target :
                     break
-                subset.append(candidates[i])
-                sum =target-  candidates[i] 
-                solve(i ,subset ,sum)
-                subset.pop()
-                
-        solve(0 ,subset,target)
+                sub.append(candidates[i])  
+                solve(i ,sub  ,target - candidates[i])   
+            sub.pop()
         return final
+        solve(0 ,sub ,target) 
+        
+        
+        
+        
+                
+        # final =[] 
+        # subset =[]
+        # candidates.sort()
+        # def solve(index ,subset  ,target):
+
+        #     if target == 0:
+        #         final.append(subset.copy())
+        #         return
+            
+        #     for i in range (index ,len(candidates)):
+        #         if candidates[i] >target :
+        #             break
+        #         subset.append(candidates[i])
+        #         sum =target-  candidates[i] 
+        #         solve(i ,subset ,sum)
+        #         subset.pop()
+                
+        # solve(0 ,subset,target)
+        # return final
 
