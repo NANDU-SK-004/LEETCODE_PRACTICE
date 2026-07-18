d4d37694acb0346ecb277e9b2a561df19d927a57
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-19 00:08:14 +0530

    Sync: 2149. Rearrange Array Elements by Sign (Medium) - Leet2Git

--- 2149_Rearrange_Array_Elements_by_Sign.py
+++ 2149_Rearrange_Array_Elements_by_Sign.py
@@ -1,8 +1,13 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        least = float("+inf")
-        max_doff =float("-inf")
-        for i in range(0 ,len(prices)):
-            least = min(least ,prices[i])
-            max_doff =max(max_doff ,prices[i] - least)
-        return max_doff
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        k =0
+        j =1
+        result =[0]*len(nums)
+        for num in nums:
+            if num >0:
+                result[k] =num
+                k+=2
+            if num < 0:
+                result[j] =num
+                j+=2
+        return result
