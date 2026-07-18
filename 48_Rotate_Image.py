1f0af2f2af22ffdee045f9d22ad11db186c35269
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-19 01:33:51 +0530

    Sync: 48. Rotate Image (Medium) - Leet2Git

--- 48_Rotate_Image.py
+++ 48_Rotate_Image.py
@@ -1,21 +1,11 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+    def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row = set()
-        col = set()
-        for i in range(0 ,len(matrix)):       #rows
-            for j in range(0 ,len(matrix[0])):   #columns
-                if matrix[i][j] == 0:
-                     row.add(i)
-                     col.add(j)
-
-        for i in row :
-            for j in range (0 ,len(matrix[0])):
-                matrix[i][j] =0
-
-        for j in col:
-            for i in range(0 ,len(matrix)):
-                matrix[i][j] =0
+        for i in range(len(matrix)):
+            for j in range(i+1 ,len(matrix[0])):
+                matrix[i][j],matrix[j][i] =matrix[j][i] ,matrix[i][j]
         
+        for i in matrix:
+            i.reverse()
