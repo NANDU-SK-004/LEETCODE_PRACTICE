9f2acb3921cfe8274c47d90f02197d38d7885a53
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-07-18 23:06:56 +0530

    Sync: 88. Merge Sorted Array (Easy) - Leet2Git

--- 88_Merge_Sorted_Array.py
+++ 88_Merge_Sorted_Array.py
@@ -1,19 +1,20 @@
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        num1 =list(set(nums1))
-        num2 =list(set(nums2))
-        i ,j =0 ,0
-        while i < len(num1) and j < len(num2):
-            if num1[i] == num2[j]:
-                i+=1
-                j+=1
-            elif num1[i] < num2[j]:
-                i+=1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                num1.insert(i ,num2[j])
-                j+=1
-        if j < len(num2):
-                num1.append(num2[j:len(num2)])
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
