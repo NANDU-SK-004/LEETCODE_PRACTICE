43e34e6b677ef7e9bc6054c06cff48d66fe4cda5
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-08-14 23:36:39 +0530

    Sync: 1431. Kids With the Greatest Number of Candies (Easy) - Leet2Git

--- 1431_Kids_With_the_Greatest_Number_of_Candies.py
+++ 1431_Kids_With_the_Greatest_Number_of_Candies.py
@@ -1,6 +1,12 @@
 class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        k =float("-inf")
-        for i in range(len(accounts)):
-            k =max(k ,sum(accounts[i]))
-        return k
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxval =max(candies)
+        ans =[0] * len(candies)
+        i=0
+        for num in candies:
+            if num + extraCandies >= maxval:
+                ans[i]=True
+            else:
+                ans[i]=False
+            i+=1
+        return ans
