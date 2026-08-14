8d6e6109dc979ed690719c98e8e949dd3f3345b8
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-08-14 22:27:17 +0530

    Sync: 1920. Build Array from Permutation (Easy) - Leet2Git

--- 1920_Build_Array_from_Permutation.py
+++ 1920_Build_Array_from_Permutation.py
@@ -1,6 +1,6 @@
 class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        if len(nums) == 0:return nums
-        for i in range (1 ,len(nums)):
-            nums[i]=nums[i-1] + nums[i]
-        return nums
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans =[0]*len(nums)
+        for i in range(len(nums)):
+            ans[i]=nums[nums[i]]
+        return ans
