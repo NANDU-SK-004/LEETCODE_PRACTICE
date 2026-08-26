3fdf2ec601fc4c559dbb793edecc1f8c354e4f82
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-08-26 19:42:22 +0530

    Sync: 344. Reverse String (Easy) - Leet2Git

--- 344_Reverse_String.py
+++ 344_Reverse_String.py
@@ -1,10 +1,11 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        if len(s)<= 1:
-            return s
-        i=0
-        while i<= len(s)//2:
-            s[i] ,s[len(s)-i-1] =s[len(s)-i-1] ,s[i]
-            i+=1
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
         return s
         
