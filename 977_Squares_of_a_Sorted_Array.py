ea9f8bc38e42119a6f577c98c04882f93f6b34f6
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-09-03 23:50:47 +0530

    Sync: 977. Squares of a Sorted Array (Easy) - Leet2Git

--- 977_Squares_of_a_Sorted_Array.py
+++ 977_Squares_of_a_Sorted_Array.py
@@ -1,7 +1,6 @@
 class Solution:
-    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        k =[]
-        for i in nums1:
-            if i in nums2:
-                k.append(i)
-        return list(set(k))        
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i]=nums[i]*nums[i]
+        nums.sort()
+        return nums
