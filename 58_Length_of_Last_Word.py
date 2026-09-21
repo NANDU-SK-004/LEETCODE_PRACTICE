ac3dc3b3fd08c41d68877aa60eb8de70c8b8c738
NANDU S KUMAR <nandanam.nandu6@gmail.com>
2026-09-21 23:03:54 +0530

    Sync: 58. Length of Last Word (Easy) - Leet2Git

--- 58_Length_of_Last_Word.py
+++ 58_Length_of_Last_Word.py
@@ -2,9 +2,9 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         count =0
         for i in range(len(s)-1 ,-1):
-            if s[i] == ' ':
+            if s[i] == " ":
                 continue
-            if s[i] != ' ':
+            if s[i] != " ":
                 count =count+1
             else :
                 break
